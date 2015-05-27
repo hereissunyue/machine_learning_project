8f87faaa2201503d73140350ffb36b7c6bb06adc
hereissunyue <hereissunyue@hotmail.com>
2015-05-27 16:48:45 -0500

    ML

---
+++ DataProcessing.m
@@ -0,0 +1,18 @@
+%% Preface
+% Name: Mahdieh Nejati Javaremi     ID: 2834041      Email:m.nejati@u.northwestern.edu
+% Name: Yue Sun                     ID: 2880445      Email:sunyue@u.northwestern.edu
+% Final Project: Sign Language Recognition with Machine Learning Algorithm
+% Description: This .m file is used for data processing
+clear
+close all
+
+
+%% Setting the path and Reading the Images
+% Dear Professor and TA, please change this path corresponding to your computer! Thank you!
+addpath('C:\Users\sunyue\Desktop\Machine Learning\Project\library')  % path of library
+addpath('C:\Users\sunyue\Desktop\Machine Learning\Project\Data') % path of image
+addpath('C:\Users\sunyue\Desktop\Machine Learning\Project\WorkSpace') % path of image
+addpath('C:\Users\sunyue\Desktop\Machine Learning\Project\Dataset') % path of image
+
+% % Read the data and Class
+load('DataSpace12740.mat');
